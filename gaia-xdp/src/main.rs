e916a59df4469590df862989c1a5a60b3a8516ec
jask <2713427119@qq.com>
2026-03-08 14:50:09 +0800

    init

---
+++ gaia-xdp/src/main.rs
@@ -0,0 +1,78 @@
+use std::net::Ipv4Addr;
+
+use anyhow::Context as _;
+use aya::{
+    maps::HashMap,
+    programs::{Xdp, XdpFlags},
+};
+use clap::Parser;
+#[rustfmt::skip]
+use log::{debug, warn};
+use tokio::signal;
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long, default_value = "eth0")]
+    iface: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let opt = Opt::parse();
+
+    env_logger::init();
+
+    // Bump the memlock rlimit. This is needed for older kernels that don't use the
+    // new memcg based accounting, see https://lwn.net/Articles/837122/
+    let rlim = libc::rlimit {
+        rlim_cur: libc::RLIM_INFINITY,
+        rlim_max: libc::RLIM_INFINITY,
+    };
+    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
+    if ret != 0 {
+        debug!("remove limit on locked memory failed, ret is: {ret}");
+    }
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Bpf::load_file` instead.
+    let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/gaia-xdp"
+    )))?;
+    match aya_log::EbpfLogger::init(&mut ebpf) {
+        Err(e) => {
+            // This can happen if you remove all log statements from your eBPF program.
+            warn!("failed to initialize eBPF logger: {e}");
+        }
+        Ok(logger) => {
+            let mut logger =
+                tokio::io::unix::AsyncFd::with_interest(logger, tokio::io::Interest::READABLE)?;
+            tokio::task::spawn(async move {
+                loop {
+                    let mut guard = logger.readable_mut().await.unwrap();
+                    guard.get_inner_mut().flush();
+                    guard.clear_ready();
+                }
+            });
+        }
+    }
+    let Opt { iface } = opt;
+    let program: &mut Xdp = ebpf.program_mut("gaia_xdp").unwrap().try_into()?;
+    program.load()?;
+    program.attach(&iface, XdpFlags::default())
+        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+    let mut blocklist: HashMap<_, u32, u32> =
+        HashMap::try_from(ebpf.map_mut("BLOCKLIST").unwrap())?;
+    let block_addr: u32 = Ipv4Addr::new(1, 1, 1, 1).into();
+    let _ = blocklist.insert(block_addr, 0, 0);
+    let block_addr2: u32 = Ipv4Addr::new(192, 168, 10, 110).into();
+    let _ = blocklist.insert(block_addr2, 0, 0);
+    let ctrl_c = signal::ctrl_c();
+    println!("Waiting for Ctrl-C...");
+    ctrl_c.await?;
+    println!("Exiting...");
+
+    Ok(())
+}
